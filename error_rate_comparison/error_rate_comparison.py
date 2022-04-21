fc4efe17dd695a3b9e381a3bbc31371bbeb7c8cb
Felix Bråberg <fillebraberg@gmail.com>
2022-04-21 08:29:36 +0200

    Added a feature to get the average difference of the sub-divided state vector

--- error_rate_comparison/error_rate_comparison.py
+++ error_rate_comparison/error_rate_comparison.py
@@ -52,6 +52,27 @@ def calc_diff_metrics(file1, file2, nr_values):
 
     return ("Max diff: ",round(max_diff, 3), "Avg. diff: ", round(tot_diff/nr_values, 5))
 
+
+## Opens a binary and produces an array of the data inside
+## Returns the array
+def binary_to_array(file1, nr_values):
+
+    with Bar("Processing", max=nr_values) as bar:
+      
+        f1 = open(file1, 'rb')
+        
+        val1arr = []
+        vary = []
+        for i in range(nr_values):
+            [val1] = struct.unpack('d', f1.read(8))
+            val1arr.append(val1)
+            vary.append(i)
+            bar.next()
+        bar.finish()
+        f1.close()
+    
+    return val1arr
+
 def run_grover_default(qubits):
     compileQuest = 'cd ../../../../QuEST/build/ && rm -rf * && cmake .. -DPRECISION=2 -DUSER_SOURCE="../examples/grovers_search.c" && make'
     os.system(compileQuest)
@@ -84,9 +105,20 @@ def run_one_test(n_qubits):
 
     # Fetch and compare metrics
     metrics = calc_diff_metrics("./grover-search_dump.data", "./grover-search_dump_no_compression.data", 2048)
-    print(metrics)
+    
+    arr_no_comp = binary_to_array("./grover-search_dump_no_compression.data", 2048)
+    arr_zfp = binary_to_array("./grover-search_dump.data", 2048)
+    sub_arrays_no_comp = sub_divide(arr_no_comp, 32)
+    sub_arrays_zfp = sub_divide(arr_zfp, 32)
 
-    return metrics
+    average_diffs = []
+
+    for i in range(len(sub_arrays_zfp)):
+        average_diffs.append(calc_avg_diff(sub_arrays_no_comp[i], sub_arrays_zfp[i]))
+        print("Average diff no." + str(i) + ":  " + str(average_diffs[i]))
+    
+    
+    
 
 """ 
     if len(sys.argv) < 4:
@@ -105,16 +137,54 @@ def run_one_test(n_qubits):
     plt.show()
     
  """
+
+## Divide an array into sub arrays of size
+## Returns array of sub arrays
+def sub_divide(array, size):
+    super_array = []
+    sub_array = []
+    cnt = 0
+    
+
+    for i in range(len(array)):
+        sub_array.append(array[i])
+        cnt += 1
+        if(cnt == size):  # Start with next sub-array
+            super_array.append(sub_array)
+            sub_array = []
+            cnt = 0
+    
+    return super_array
+            
+
+
+
+## Calculates the average difference between two numerical arrays
+## Returns the average difference
+def calc_avg_diff(arr1, arr2):
+
+    if(len(arr1) != len(arr2)):  # Invalid sizing
+        return -1
+
+    cummulative_diff = 0
+    length = len(arr1)
+
+    for i in range(length):
+        cummulative_diff += abs(arr1[i] - arr2[i])
+
+    return cummulative_diff/length
+
     
 def main():
+    print("Test")
+    # Calc avg. for sections and calc error rate metrics
+    # X-axis = section idx
+    # Y-axis = diff
 
     run_one_test(10)
-    max_diff_list = []
-    avg_diff_list = []
-
-
     
-
+    
+   
     """
     for qubits in range(10, 12):
         result = run_one_test(qubits)
